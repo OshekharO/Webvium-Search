1d4f77a8cbb7c000f262c8a54b3010c0c14581d5
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2022-02-23 13:48:16 +0530

    Create search.js

---
+++ search.js
@@ -0,0 +1,228 @@
+/*
+*
+* Copyright (c) 2022 Melvin Jones Repol (mrepol742.github.io). All rights reserved.
+*
+* License under the GNU General Public License, Version 3.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     https://www.gnu.org/licenses/gpl-3.0.en.html
+*
+* Unless required by the applicable law or agreed in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+lozad().observe();
+var defSH = "https://source.unsplash.com/";
+
+try {
+	if (Webvium.currentVersion() >= 28) {
+		var bgn = WebviumThemeHelper.getBackgroundStatus();
+		if (bgn == "60z") {
+			bgde();
+		} else if (bgn == "30z") {
+			if (!WebviumThemeHelper.isCustomBackgroundEnabled()) {
+				if (WebviumThemeHelper.isDarkModeEnabled()) {
+					setColor("#000");
+				} else {
+					setColor("#fff");
+				}
+			} else {
+				trans();
+			}
+		} else if (bgn == "7z") {
+			trans();
+		} else {
+			if (!WebviumThemeHelper.isCustomBackgroundEnabled()) {
+				var night = "https://mrepol742.github.io/images/webvium-";
+				if (WebviumThemeHelper.isDarkModeEnabled()) {
+					//if (Webvium.isDev()) {
+						setBg(night + "dev-bg-dark.jpg");
+					//} else {
+				    //  setBg(night + "bg-dark.jpg");
+					//}
+				} else {
+					//if (Webvium.isDev()) {
+						setBg(night + "dev-bg-light.jpg");
+					//} else {
+					//  setBg(night + "bg-light.jpg");
+					//}
+				}
+			} else {
+				trans();
+			}
+		}
+	} else {
+		bgde();
+	}
+} catch (a) {
+	setBg(defSH + "640x480?day");
+}
+
+function bgde() {
+	if (!WebviumThemeHelper.isCustomBackgroundEnabled()) {
+		if (WebviumThemeHelper.isDarkModeEnabled()) {
+			setBg(defSH + WebviumThemeHelper.getQuality() + "?night");
+		} else {
+			setBg(defSH + WebviumThemeHelper.getQuality() + "?day");
+		}
+	} else {
+		trans();
+	}
+}
+
+function setBg(a) {
+	document.body.style.background = "url('" + a + "') no-repeat fixed center";
+	document.body.style.backgroundSize = "cover";
+}
+
+function trans() {
+	document.body.style.background = "transparent";
+}
+
+function setColor(a) {
+	document.body.style.backgroundColor = a;
+}
+
+const node = document.getElementById("search");
+node.addEventListener("keyup", function (event) {
+	if (event.key === "Enter") {
+		find(search.value);
+	}
+});
+
+/*
+ * Roses are red
+ * Violets are blue
+ * If you are reading this
+ * You are an idiot too
+*/
+
+
+try {
+	var su = WebviumSearchHelper.query().split(":");
+	if (su != "null") {
+		for (let i = 0; i < su.length; i++) {
+			let sugItem = document.createElement('div');
+			sugItem.setAttribute('class', 'sug-item');
+
+			let icon = document.createElement('span');
+			icon.setAttribute('class', 'fa fa-search');
+
+			let text = document.createElement('span');
+			text.setAttribute('class', 'text');
+			text.textContent = atob(su[i]);
+
+			sugItem.appendChild(icon);
+			sugItem.appendChild(text);
+			sug.appendChild(sugItem);
+
+			search.addEventListener('input', () => {
+				if (search.value.length != 0) {
+					sug.style.display = 'flex';
+					if (text.textContent.includes(search.value)) {
+						sugItem.style.display = 'flex';
+						sug.style.display = 'flex';
+					} else {
+						sugItem.style.display = 'none';
+					}
+
+					sugItem.addEventListener('click', () => {
+						search.value = atob(su[i]);
+					});
+				} else {
+					sug.style.display = 'none';
+					search.style.transform = 'translateY(0%)';
+					webvium.style.transform = 'translateY(0%)';
+					sug.style.transform = 'tranlate(-50%, 0%)';
+				}
+			});
+		}
+	}
+} catch (a) { }
+
+search.addEventListener('input', () => {
+	try {
+		if (WebviumSearchSuggestion.isSearchSuggestionsEnabled()) {
+			var inp = search.value;
+			if (inp.trim().length != 0) {
+				let divs = document.querySelectorAll('.sug-item');
+
+				for (let i = 0; i < divs.length; i++) {
+					sug.removeChild(divs[i]);
+				}
+
+				sug.style.display = 'flex';
+				var doc = WebviumSearchSuggestion.getResponse(search.value);
+				var jsonString = xml2json($.parseXML(doc)).split('undefined').join('');
+				var json = JSON.parse(jsonString);
+				var topLevel = json.toplevel;
+				var comsug = topLevel.CompleteSuggestion;
+				var data = "";
+
+				for (let i = 0; i < comsug.length; i++) {
+					data += comsug[i].suggestion.data + "<br>";
+					let sugItem = document.createElement('div');
+					sugItem.setAttribute('class', 'sug-item');
+
+					let icon = document.createElement('span');
+					icon.setAttribute('class', 'fa fa-search');
+
+					let text = document.createElement('span');
+					text.setAttribute('class', 'text');
+					text.textContent = comsug[i].suggestion.data;
+
+					sugItem.appendChild(icon);
+					sugItem.appendChild(text);
+					sug.appendChild(sugItem);
+					sugItem.addEventListener('click', () => {
+						search.value = comsug[i].suggestion.data;
+						sug.style.display = 'none';
+						find(comsug[i].suggestion.data);
+					});
+				}
+			} else {
+				sug.style.display = 'none';
+				sug.innerHTML = '';
+			}
+		}
+	} catch (ex) { }
+});
+
+function find(query) {
+	try {
+		let t = query;
+		if (t.trim()) {
+			const a = document.getElementById("search").value;
+			const aq = a.toLowerCase();
+			if (aq.startsWith("https://") || aq.startsWith("http://")) {
+				if (WebviumSearchHelper.isValidDomain(aq)) {
+					window.location.href = a;
+				} else {
+					window.location.href = WebviumSearchHelper.getSearchEngine() + a;
+				}
+			} else {
+				if (WebviumSearchHelper.isValidDomain(aq)) {
+					window.location.href = "https://" + a;
+				} else {
+					window.location.href = WebviumSearchHelper.getSearchEngine() + a;
+				}
+			}
+			WebviumSearchHelper.saveQuery(a);
+		}
+	} catch (qw) {
+		let t = search.value;
+		if (t.trim()) {
+			const a = document.getElementById("search").value;
+			const aq = a.toLowerCase();
+			if (aq.startsWith("https://") || aq.startsWith("http://")) {
+				window.location.href = a;
+			} else {
+				window.location.href = "https://google.com/search?q=" + a;
+			}
+		}
+	}
+}
